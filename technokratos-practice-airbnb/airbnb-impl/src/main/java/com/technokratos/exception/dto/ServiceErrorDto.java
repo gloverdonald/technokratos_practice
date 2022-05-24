d518f2d8840e727efd81d0803a4393895fec5e0c
Milana Raimanova <20milana02@gmail.com>
2022-05-24 19:58:58 +0300

    add validation

---
+++ technokratos-practice-airbnb/airbnb-impl/src/main/java/com/technokratos/exception/dto/ServiceErrorDto.java
@@ -0,0 +1,21 @@
+package com.technokratos.exception.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ServiceErrorDto {
+
+    private String field;
+
+    private String exception;
+
+    private String message;
+}
